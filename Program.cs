7dc111a4f8c15c04a4bf47309fc2aa331fa65b3d
render01 <54182692+render01@users.noreply.github.com>
2020-11-01 16:15:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//Использование объекта Mutex для создания приложения, исполняемого в единственном экземпляре
+using System;
+using System.Threading;
+
+namespace prak8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex oneMutex = null;
+            const String MutexName = "RUNMEONCE";
+            try
+            {
+                oneMutex = Mutex.OpenExisting(MutexName);
+            }
+            catch (WaitHandleCannotBeOpenedException)
+            {
+            }
+            if (oneMutex == null)
+            {
+                oneMutex = new Mutex(true, MutexName);
+            }
+            else
+            {
+                oneMutex.Close();
+                return;
+            }
+            Console.WriteLine("Our Application");
+            Console.Read();
+        }
+    }
+}
